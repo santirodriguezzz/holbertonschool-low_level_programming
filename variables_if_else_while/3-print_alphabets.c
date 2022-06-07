bd2cb2c1c2de5bb1183cc603518bfc213f118d81
santirodriguezzz <santiagorodri130898@gmail.com>
2022-06-07 16:54:30 -0700

    task2

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ *main - Entry point
+ *
+ *Return: Always 0 (Succes)
+ */
+
+
+int main(void)
+{
+	char n;
+	for (n = 'A'; n <= 'Z'; n++)
+	{
+	putchar(n);
+	}
+	putchar('\n');
+return (0);
+}
