a9bd65743485f035688d7386c740251fce388c15
santirodriguezzz <santiagorodri130898@gmail.com>
2022-06-07 17:12:34 -0700

    task3

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -11,11 +11,13 @@
 int main(void)
 {
 	char n;
-	for (n = 'A'; n <= 'Z'; n++)
-	for (n = 'a'; n <= 'z';	n++)
+	for (n = 'a'; n <= 'z'; n++)
 	{
 	putchar(n);
 	}
+	for (n = 'A'; n <= 'Z'; n++)
+	putchar(n);
 	putchar('\n');
+
 return (0);
 }
