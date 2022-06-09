4f8475913afdec047586aec6d98fd0d66a0ed2ee
santirodriguezzz <santiagorodri130898@gmail.com>
2022-06-09 12:48:31 -0700

    task2

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+* print_alphabet - print a,b,c,... for 10 times
+*
+* Return: Always 0.
+*/
+void print_alphabet_x10(void)
+{
+	int n;
+	char c;
+
+	for (n = 0; n <= 9; n++)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+	}
+}
