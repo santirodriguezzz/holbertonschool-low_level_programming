b3bbc1389c63f5a4d3a9bf53dcc2b99df7a69608
santirodriguezzz <santiagorodri130898@gmail.com>
2022-06-10 07:28:48 -0700

    task 4

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * _isalpha - function that checks for alphabetic character
- *@c variable storing value to be checked
+ *@c: variable storing value to be checked
  *Return: 1 if c is a letter, 0 if otherwise
  */
 int _isalpha(int c)
