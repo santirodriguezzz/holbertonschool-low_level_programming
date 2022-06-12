a6d221f8356bc2f36e820c58a7ca069ce6d8c360
santirodriguezzz <santiagorodri130898@gmail.com>
2022-06-12 16:01:41 -0700

    task10

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add - funcion que suma dos numeros enteros y devuelve el resultado
+ * @a: variable que almacena el primer int que se agrega
+ * @b: variable que alacena el segundo int que se agrega
+ * Return: resultado operacion
+ */
+int add(int a, int b)
+{
+	int res;
+
+	res = a + b;
+	return (res);
+}
