7ab23a1265e06c78a2f3087468e1a2b520d4103f
santirodriguezzz <santiagorodri130898@gmail.com>
2022-06-12 19:39:59 -0700

    task9

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - test fizz buzz 
+ *
+ * Return: 0;
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i < 101; i++)
+	{
+		if ((i % 3 == 0) && (i % 5 == 0))
+		{
+			printf("FizzBuzz");
+			putchar(32);
+		}
+		else if ((i % 3) == 0)
+		{
+			printf("Fizz");
+			putchar(32);
+		}
+		else if ((i % 5) == 0)
+		{
+			printf("Buzz");
+			if (i < 100)
+			{
+				putchar(32);
+			}
+		}
+		else
+		{
+			printf("%d", i);
+			putchar(32);
+		}
+	}
+	putchar(10);
+	return (0);
+}
