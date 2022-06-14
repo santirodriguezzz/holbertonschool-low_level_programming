0a1abefc75efbee51491f23554b3c22486a4f2bf
santirodriguezzz <santiagorodri130898@gmail.com>
2022-06-14 14:08:46 -0700

    task2

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *  _strlen - funcion que muestre la longitud de string
+ *  @s: variable string
+ *  Return: largo del string
+ */
+int _strlen(char *s)
+{
+	int count = 0;
+	while (*s != '\0')
+	{
+		count++;
+		s++;
+	}
+	return (count);
+}
