b40e7251d6d57e99bbf497b603e7c0f7875c54d9
santirodriguezzz <santiagorodri130898@gmail.com>
2022-06-16 09:11:06 -0700

    task6

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * puts2 - funcion que muestra los caracteres desde el principio uno si y uno no.
+ *
+ * @str: variable
+ */
+void puts2(char *str)
+{
+	int i, x;
+
+	for (x = 0; str[x] != '\0'; x++)
+	{}
+	for (i= 0; i < x; i = i + 2)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+
+}
