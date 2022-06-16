8c42490edca571ebafd2beb7ba50fd548ae6d02b
santirodriguezzz <santiagorodri130898@gmail.com>
2022-06-16 11:18:26 -0700

    task7

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+ *  _strlen - funcion que muestre la longitud de string
+ *  @s: variable string
+ *  Return: largo del string
+ */
+int _strlen(char *s)
+{
+	int count = 0;
+
+	while (*s != '\0')
+	{
+		count++;
+		s++;
+	}
+	return (count);
+}
+
+/**
+ * puts_half - funcion que imprima la mitad de una cadena + \n
+ *
+ * @str: variable string
+ */
+void puts_half(char *str)
+{
+	int n, l;
+
+	l = _strlen(str);
+
+	for ( n = l / 2; str[n] != '\0'; n++)
+	{
+		_putchar (str[n]);
+	} 
+		_putchar ('\n');
+
+}
