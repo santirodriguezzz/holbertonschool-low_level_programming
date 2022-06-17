a806ec85a39df49c0d38636bce5ffc08a1a70e3f
santirodriguezzz <santiagorodri130898@gmail.com>
2022-06-17 11:37:32 -0700

    task1

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * *_strncat -funcion que concatena 2 strings con un tope
+ *
+ * @dest: variable string que recibe datos
+ * @src: variable string que envia datos
+ * @n: tope
+ * Return: dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0, x;
+
+	for (i = 0; dest[i]; i++)
+	;
+
+	for (x = 0; x < n; x++)
+	{
+		dest[i] = src[x];
+		i++;
+	}
+	return (dest);
+}
