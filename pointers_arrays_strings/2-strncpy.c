a2d78492653b34d6bd4773cfa5b46001d620a57a
santirodriguezzz <santiagorodri130898@gmail.com>
2022-06-17 19:03:44 -0700

    task2

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -1,9 +1,10 @@
 #include "main.h"
 /**
- * *_strncpy - funcion que copia dos string
+ * _strncpy - funcion que copia dos string
  * @dest:variable string
  * @src:variable string
  * @n:variable cantidad de largo a copiar
+ *Return: devuelve dest
  */
 char *_strncpy(char *dest, char *src, int n)
 {
@@ -12,10 +13,12 @@ char *_strncpy(char *dest, char *src, int n)
 	if (dest[0] != '\0' && src[i] != '\0')
 	{
 
-		for (i = 0; i < n; i++)
+		for (i = 0; i < n && src[i] != '\0'; i++)
 		{
 			dest[i] = src[i];
 		}
+		for ( ; i < n; i++)
+			dest[i] = '\0';
 	}
 		return (dest);
 }
