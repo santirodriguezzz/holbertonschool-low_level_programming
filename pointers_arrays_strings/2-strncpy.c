891d6bcd080fa9697b0126c60c0f49821d7eb47e
santirodriguezzz <santiagorodri130898@gmail.com>
2022-06-17 19:17:24 -0700

    task2

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -10,15 +10,12 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i = 0;
 
-	if (dest[0] != '\0' && src[i] != '\0')
-	{
 
-		for (i = 0; i < n && src[i] != '\0'; i++)
-		{
-			dest[i] = src[i];
-		}
-		for ( ; i < n; i++)
-			dest[i] = '\0';
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
 	}
-		return (dest);
+	for ( ; i < n; i++)
+		dest[i] = '\0';
+	return (dest);
 }
