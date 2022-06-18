9a071b986a88dba03129988058ed6ef1b24f4cdb
santirodriguezzz <santiagorodri130898@gmail.com>
2022-06-17 20:37:15 -0700

    task3

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _strcmp - funcion que compara vos string
+ * @s1: variable string 1
+ * @s2: variable string 2
+ *Return: retorna 0
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0;
+
+	while (s1[i] != s2[i])
+	{
+		if (s1[1] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
+	i++;
+	}
+	return (0);
+}
