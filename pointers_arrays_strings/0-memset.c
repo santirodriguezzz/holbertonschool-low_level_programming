b625985d4cfc986f74a0dc74eae2f76e5cb5d0b4
santirodriguezzz <santiagorodri130898@gmail.com>
2022-06-19 15:03:39 -0700

    task0

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * memset - funcion que llena la memoria con un byte constante
+ * @n: numero de bytes en el area de memoria
+ * @s: string
+ * @b: byte constante
+ * Return: puntero
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
