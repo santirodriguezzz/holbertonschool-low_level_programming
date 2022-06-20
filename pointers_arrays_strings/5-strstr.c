e278cbe7bc266b2ce22626df3f5a6369b142244b
santirodriguezzz <santiagorodri130898@gmail.com>
2022-06-19 17:08:54 -0700

    task5

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* _strstr - encuentra la condicion
+* @needle: substring a encontrar
+* @haystack: string para comprobar 
+* Return: puntero 
+*/
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j;
+
+	for (i = 0; haystack[i]; i++)
+	{
+		for (j = 0; needle[j]; j++)
+		{
+			if (needle[j] != haystack[i + j])
+				break;
+		}
+		if (needle[j] == '\0')
+			return (&haystack[i]);
+	}
+	return (NULL);
+}
