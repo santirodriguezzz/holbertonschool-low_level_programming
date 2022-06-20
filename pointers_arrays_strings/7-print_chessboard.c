a5f1495cb3dad849bebb5de33033300c5545a047
santirodriguezzz <santiagorodri130898@gmail.com>
2022-06-19 17:47:31 -0700

    task6

---
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* print_chessboard - imprime tablero ajedres
+* @a: valores para imprimir 
+*/
+void print_chessboard(char (*a)[8])
+{
+	int row, col;
+
+	for (row = 0; row < 8; row++)
+	{
+		for (col = 0; col < 8; col++)
+		{
+			printf("%c", a[row][col]);
+		}
+		printf("\n");
+	}
+}
