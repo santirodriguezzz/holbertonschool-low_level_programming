9645265d815c8bf4577eeb11bed3001633bed72d
santirodriguezzz <santiagorodri130898@gmail.com>
2022-06-21 15:25:09 -0700

    task2

---
+++ argc_argv/2-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - imprime numero de argumentos
+ * @argc: numero argumentos
+ * @argv: unused
+ * Return: siempre 0
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return(0);
+}
