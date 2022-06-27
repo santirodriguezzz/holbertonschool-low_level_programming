b4f95beeb5239c46635e02842dfb895b51446afa
santirodriguezzz <santiagorodri130898@gmail.com>
2022-06-26 19:28:55 -0700

    task2

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+/**
+* _calloc - funcion que asigna memoria para un array
+* @nmemb: numero de elementos
+* @size: cantidadde bytes
+* Return: puntero al array 
+*/
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *array;
+
+	if (size == 0 || nmemb == 0)
+		return (NULL);
+	array = malloc(nmemb * size);
+	if (array == NULL)
+	return (NULL);
+	memset(array, 0, nmemb * size);
+	return (array);
+}
