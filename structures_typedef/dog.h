4db0d9965848e97dfdc5f1b4a28d8b9080335ad2
santirodriguezzz <santiagorodri130898@gmail.com>
2022-06-28 15:43:30 -0700

    task0

---
+++ structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+* struct dog - poppy
+* @name : nombre perro
+* @owner : dueño 
+* @age : edad
+*/
+struct dog
+	{
+	char *name;
+	float age;
+	char *owner;
+	};
+#endif
