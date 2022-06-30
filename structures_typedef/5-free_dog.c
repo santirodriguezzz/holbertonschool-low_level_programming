951aa4b9ec94ebe2b1d511902b97b67a80f30a36
santirodriguezzz <santiagorodri130898@gmail.com>
2022-06-29 19:12:27 -0700

    task5

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+/**
+* free_dog
+* @d:
+*/
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return;
+	free(d->owner);
+	free(d->name);
+	free(d);
+}
