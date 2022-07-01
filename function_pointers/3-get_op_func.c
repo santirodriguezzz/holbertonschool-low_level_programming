6f2b1ba8aad965d3a1118cc672633fd1e657d404
santirodriguezzz <santiagorodri130898@gmail.com>
2022-06-30 18:40:04 -0700

    task3

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -1,7 +1,11 @@
 #include <stddef.h>
 #include <stdlib.h>
 #include "3-calc.h"
-
+/**
+* get_op_func - funcion
+* @s: char
+* Return: 0
+*/
 int (*get_op_func(char *s))(int, int)
 {
 	op_t ops[] = {
@@ -15,7 +19,7 @@ int (*get_op_func(char *s))(int, int)
 	int i;
 
 	i = 0;
-	while(ops[i].op)
+	while (ops[i].op)
 	{
 		if (ops[i].op[0] == *s)
 			return (ops[i].f);
