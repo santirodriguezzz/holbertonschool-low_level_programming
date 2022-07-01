24dd8ae281199066c198caed4646dc9744b24463
santirodriguezzz <santiagorodri130898@gmail.com>
2022-06-30 18:14:02 -0700

    task3

---
+++ function_pointers/3-main.c
@@ -0,0 +1,32 @@
+#include <stddef.h>
+#include <stdio.h>
+#include "3-calc.h"
+#include <stdlib.h>
+/**i
+ * main - funcion
+ * @argc: int 
+ * @argv: int
+ * Return: 0
+ */
+
+int main (int argc, char *argv[])
+{
+	int (*x)(int, int);
+	int a, b;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	x = get_op_func(argv[2]);
+	if (x == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+	printf("%d\n", x(a, b));
+	return (0);
+}
