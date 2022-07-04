45df5cde99f2d47b9f656e1e875ca4d47c40f506
santirodriguezzz <santiagorodri130898@gmail.com>
2022-07-03 18:18:11 -0700

    task2

---
+++ variadic_functions/2-print_strings.c
@@ -0,0 +1,26 @@
+#include "variadic_functions.h"
+/**
+* print_strings - muestra string
+* @separator: string
+* @n: numero arg
+*/
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int x;
+	char *arg;
+
+	va_start(ap, n);
+	for (x = 0; x < n; x++)
+	{
+		arg = va_arg(ap, char*);
+		if (arg == NULL)
+			printf("(nil)");
+		else
+			printf("%s", arg);
+		if (separator != NULL && x < n - 1)
+			printf("%s", separator);
+	}
+	va_end(ap);
+	printf("\n");	
+}
