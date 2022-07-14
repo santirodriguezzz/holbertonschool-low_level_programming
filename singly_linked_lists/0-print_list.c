09b72ba3150f30ef8613733efd4231ed37be4b38
santirodriguezzz <santiagorodri130898@gmail.com>
2022-07-14 09:21:52 -0700

    task0

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/*
+* print_list -
+* @list_t:
+* @h
+**/
+
+size_t print_list(const list_t *h)
+{
+	int count = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%i] %s\n", h->len, h->str);
+		count++;
+		h = h->next;
+	
+	}
+	return (count);
+}
