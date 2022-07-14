6e57937c94b2e4b5bb742ce1009ae572e6043b72
santirodriguezzz <santiagorodri130898@gmail.com>
2022-07-14 12:04:24 -0700

    task 0

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+ size_t list_len(const list_t *h)
+{
+	int count = 0;
+
+	while (h)
+	{
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
