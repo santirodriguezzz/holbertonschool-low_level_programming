d7757bf4e258eb40c230b2a809e8d323e338d418
santirodriguezzz <santiagorodri130898@gmail.com>
2022-07-14 15:45:05 -0700

    task 3

---
+++ singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+/**
+ * add_node_end - function
+ * @head: first node
+ * @str: string
+ * Return: newNode
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *newNode;
+	list_t *lastNode = *head;
+
+	newNode = malloc(sizeof(list_t));
+	if (newNode)
+		return (NULL);
+	newNode->str = strdup(str);
+	newNode->len = strlen(str);
+	newNode->next = NULL;
+	if (*head == NULL)
+	{
+		*head = newNode;
+		return (newNode);
+	}
+	while (lastNode->next)
+		lastNode = lastNode->next;
+	lastNode->next = newNode;
+	return (newNode);
+}
