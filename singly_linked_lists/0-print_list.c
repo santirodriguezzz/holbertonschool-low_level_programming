9f565185373752134bd101c67951490bd9ad5122
santirodriguezzz <santiagorodri130898@gmail.com>
2022-07-14 09:32:26 -0700

    task 0

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,9 +1,9 @@
 #include "lists.h"
-/*
-* print_list -
-* @list_t:
-* @h
-**/
+/**
+ * print_list - function that print a node
+ * @h: pointer
+ * Return:  the number of nodes
+ */
 
 size_t print_list(const list_t *h)
 {
@@ -17,7 +17,6 @@ size_t print_list(const list_t *h)
 			printf("[%i] %s\n", h->len, h->str);
 		count++;
 		h = h->next;
-	
 	}
 	return (count);
 }
