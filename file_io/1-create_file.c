5b14f2fe6a10df3de544bc8b5303a86c4009f9a8
santirodriguezzz <santiagorodri130898@gmail.com>
2022-07-20 15:44:12 -0700

    task1

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -9,7 +9,7 @@ int create_file(const char *filename, char *text_content)
 {
 	int fd, count = 0;
 
-	fd = open(filename, O_CREAT | O_WRONLY, 0600);
+	fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
 	if (fd == -1 || !filename)
 		return (-1);
 	while (text_content[count])
