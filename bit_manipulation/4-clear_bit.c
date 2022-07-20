0e19c0299b29038feaafac182ca16b8c32f4bbd3
santirodriguezzz <santiagorodri130898@gmail.com>
2022-07-19 17:40:06 -0700

    task4

--- bit_manipulation/4-clear_bit.c
+++ bit_manipulation/4-clear_bit.c
@@ -1,14 +1,15 @@
 #include "main.h"
 /**
-*clear_bit -
-*@n:
-*@index:
+*clear_bit - function
+*@n: value of array
+*@index: position of array
+*Return: suces
 */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
 	if (n)
 	{
-		if (index <= 64)
+		if (index <= 63)
 		{
 			*n &= -(1 << index);
 			return (1);
