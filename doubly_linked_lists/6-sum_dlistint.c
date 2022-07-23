4de4d11af890318a4b445590800d67e5e914a3b0
santirodriguezzz <santiagorodri130898@gmail.com>
2022-07-22 18:13:46 -0700

    task6

---
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+* sum_dlistint - function that returns the sum of all the data (n
+* @head: pointer to head node
+* Return: sum of all the data
+*/
+int sum_dlistint(dlistint_t *head)
+{
+	int sum = 0;
+
+	while (head)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+	return (sum);
+}
