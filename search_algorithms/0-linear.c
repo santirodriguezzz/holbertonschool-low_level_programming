e67b461bb8707e7daf0ede7091685035f3ae5dbd
santirodriguezzz <santiagorodri130898@gmail.com>
2022-08-26 11:46:25 -0700

    betty fixes

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -1,11 +1,11 @@
 #include "search_algos.h"
 
 /**
- * linear_search -
- * @array:
- * @size:
- * @value:
- * Retrun:
+ * linear_search - function that searches for a value in an array
+ * @array: first element in array
+ * @size: number of elements in array
+ * @value: is the value to search for
+ * Retrun: first index where value is located
  */
 
 int linear_search(int *array, size_t size, int value)
